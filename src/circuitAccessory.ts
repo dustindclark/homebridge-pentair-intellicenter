c0aebad4e51be983c70a6094c412c8ee3535e6e0
Dustin Clark <dustin@captivatelabs.com>
2021-11-29 00:10:45 -0600

    Undefined check on getOn() status.

--- src/circuitAccessory.ts
+++ src/circuitAccessory.ts
@@ -94,6 +94,6 @@ export class CircuitAccessory {
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
   async getOn(): Promise<CharacteristicValue> {
-    return this.accessory.context.status!.params && this.accessory.context.status.params[STATUS_KEY] === CircuitStatus.On;
+    return this.accessory.context!.status!.params && this.accessory.context.status.params[STATUS_KEY] === CircuitStatus.On;
   }
 }
